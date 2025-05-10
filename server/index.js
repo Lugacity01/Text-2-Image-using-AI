82e693ae4856447f79c63ce3a1c6f9e76ff97fc4
Lugacity01 <yinkaabeebadesina@gmail.com>
2025-05-10 12:44:13 +0100

    vercel component reps

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { createServer } from "http";
 
 import connectDB from "./config/mongodb.js";
 import userRouter from "./routes/userRoutes.js";
 import imageRouter from "./routes/imageRoutes.js";
 import verifyPaystack from "./routes/verifyPaystack.js";
+// import userRouter from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-
+app.use(cors());
 await connectDB();
 
 app.use("/api/user", userRouter);
@@ -22,16 +21,4 @@ app.use("/api/user/verify", verifyPaystack);
 
 app.get("/", (req, res) => res.send("API Working"));
 
-// ✅ Local development
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log("Server running locally on port " + PORT);
-  });
-}
-
-// ✅ Vercel serverless support
-const server = createServer(app);
-
-export default function handler(req, res) {
-  server.emit("request", req, res);
-}
+app.listen(PORT, () => console.log("Server  running on port" + PORT));
